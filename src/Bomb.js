d819352360cc5bc16d3527b174d79097011c7704
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-11 21:24:03 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class Bomb extends Component{
 
+  constructor(props){
+    super(props)
+    this.state = {
+      secondsLeft: this.props.initialCount;
+    }
+  }
   render(){
     return
   }
