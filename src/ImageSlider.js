af3f99ba34d896c746db283f3f0b7ff28fa2c5a1
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-11 21:18:47 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
 class ImageSlider extends Component{
-
+  constructor(props){
+    super(props)
+    this.state = {
+      currentSlideIndex = 0
+    }
+  }
   render(){
     return
   }
